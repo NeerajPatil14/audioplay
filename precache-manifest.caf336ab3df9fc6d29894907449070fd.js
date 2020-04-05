1b1a52f27aa22740fc7c2b154b4d18926ceccbd6
Neeraj Patil <neerajpatil14@gmail.com>
2020-04-05 15:11:08 +0530

    Updates

---
+++ precache-manifest.caf336ab3df9fc6d29894907449070fd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7f2144d82894fbe6bfccc02e4cd6be",
+    "url": "/audioplay/index.html"
+  },
+  {
+    "revision": "099f2b72ba9605211066",
+    "url": "/audioplay/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dad5718f65e42df882c2",
+    "url": "/audioplay/static/js/2.83511e14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/audioplay/static/js/2.83511e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099f2b72ba9605211066",
+    "url": "/audioplay/static/js/main.5c332ae4.chunk.js"
+  },
+  {
+    "revision": "24674aa19e54ea64a509",
+    "url": "/audioplay/static/js/runtime-main.87b165a9.js"
+  },
+  {
+    "revision": "441a91a419cd68466fc792254290e0df",
+    "url": "/audioplay/static/media/img1.441a91a4.jpg"
+  },
+  {
+    "revision": "0a12c3548340a69d7d2519085f1b96c9",
+    "url": "/audioplay/static/media/img10.0a12c354.jpg"
+  },
+  {
+    "revision": "fddcd714b1de343bb03acbb7453bd9e2",
+    "url": "/audioplay/static/media/img2.fddcd714.jpg"
+  },
+  {
+    "revision": "1fe82a14cf2763f7250fb4d8015e1ae3",
+    "url": "/audioplay/static/media/img3.1fe82a14.jpg"
+  },
+  {
+    "revision": "b3d81d9ac23006bdbfcd4cb6dda43c08",
+    "url": "/audioplay/static/media/img4.b3d81d9a.jpg"
+  },
+  {
+    "revision": "d4faa9cf061dd5aea20e18402bc75322",
+    "url": "/audioplay/static/media/img5.d4faa9cf.jpg"
+  },
+  {
+    "revision": "08aca987b54f114502679bf414983f30",
+    "url": "/audioplay/static/media/img6.08aca987.jpg"
+  },
+  {
+    "revision": "32d686c6378128c251cd2c9b1e1bf30d",
+    "url": "/audioplay/static/media/img7.32d686c6.jpg"
+  },
+  {
+    "revision": "8ad2c3752322f2069bc8439eb0f02af0",
+    "url": "/audioplay/static/media/img8.8ad2c375.jpg"
+  },
+  {
+    "revision": "3734bcf0ceb338022aa17725ffd23a42",
+    "url": "/audioplay/static/media/img9.3734bcf0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/audioplay/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f62081bb6dd62fd9aea1e5aa635350e",
+    "url": "/audioplay/static/media/part1.1f62081b.mp3"
+  },
+  {
+    "revision": "d9f20caa89e0f1529eacb5c47b3e5ec7",
+    "url": "/audioplay/static/media/part10.d9f20caa.mp3"
+  },
+  {
+    "revision": "3097ec027164c619d31085dd8f0279a0",
+    "url": "/audioplay/static/media/part2.3097ec02.mp3"
+  },
+  {
+    "revision": "d9c424af95c6fcfe70558fdb444d00ce",
+    "url": "/audioplay/static/media/part3.d9c424af.mp3"
+  },
+  {
+    "revision": "730a696508b9a05cab11a36a2862ce30",
+    "url": "/audioplay/static/media/part4.730a6965.mp3"
+  },
+  {
+    "revision": "cb710e0b0d113f4d607bcfec2b6b0ff1",
+    "url": "/audioplay/static/media/part5.cb710e0b.mp3"
+  },
+  {
+    "revision": "59773c02a97f5d122ec085de87f70eb5",
+    "url": "/audioplay/static/media/part6.59773c02.mp3"
+  },
+  {
+    "revision": "bfc49738baa2c637c1d066d78902de1b",
+    "url": "/audioplay/static/media/part7.bfc49738.mp3"
+  },
+  {
+    "revision": "8833d65d208356cce4f6e16df958b31f",
+    "url": "/audioplay/static/media/part8.8833d65d.mp3"
+  },
+  {
+    "revision": "7233439a29fd0b6c097f576fb292d131",
+    "url": "/audioplay/static/media/part9.7233439a.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/audioplay/precache-manifest.caf336ab3df9fc6d29894907449070fd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/audioplay/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
