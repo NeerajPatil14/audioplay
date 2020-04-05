e278100666bd2b3e2df8e8e258d7ff3bdf4642d3
Neeraj Patil <neerajpatil14@gmail.com>
2020-04-05 14:19:12 +0530

    First commit

---
+++ build/precache-manifest.5fbda9a809e3f526e46d44d2ba3fd281.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8e21c950863daff51dd7a7904ff856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7326c1a75b392d33ef90",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dad5718f65e42df882c2",
+    "url": "/static/js/2.83511e14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.83511e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7326c1a75b392d33ef90",
+    "url": "/static/js/main.0434b6f1.chunk.js"
+  },
+  {
+    "revision": "c68944ddea2739b5341a",
+    "url": "/static/js/runtime-main.17c16628.js"
+  },
+  {
+    "revision": "441a91a419cd68466fc792254290e0df",
+    "url": "/static/media/img1.441a91a4.jpg"
+  },
+  {
+    "revision": "0a12c3548340a69d7d2519085f1b96c9",
+    "url": "/static/media/img10.0a12c354.jpg"
+  },
+  {
+    "revision": "fddcd714b1de343bb03acbb7453bd9e2",
+    "url": "/static/media/img2.fddcd714.jpg"
+  },
+  {
+    "revision": "1fe82a14cf2763f7250fb4d8015e1ae3",
+    "url": "/static/media/img3.1fe82a14.jpg"
+  },
+  {
+    "revision": "b3d81d9ac23006bdbfcd4cb6dda43c08",
+    "url": "/static/media/img4.b3d81d9a.jpg"
+  },
+  {
+    "revision": "d4faa9cf061dd5aea20e18402bc75322",
+    "url": "/static/media/img5.d4faa9cf.jpg"
+  },
+  {
+    "revision": "08aca987b54f114502679bf414983f30",
+    "url": "/static/media/img6.08aca987.jpg"
+  },
+  {
+    "revision": "32d686c6378128c251cd2c9b1e1bf30d",
+    "url": "/static/media/img7.32d686c6.jpg"
+  },
+  {
+    "revision": "8ad2c3752322f2069bc8439eb0f02af0",
+    "url": "/static/media/img8.8ad2c375.jpg"
+  },
+  {
+    "revision": "3734bcf0ceb338022aa17725ffd23a42",
+    "url": "/static/media/img9.3734bcf0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f62081bb6dd62fd9aea1e5aa635350e",
+    "url": "/static/media/part1.1f62081b.mp3"
+  },
+  {
+    "revision": "d9f20caa89e0f1529eacb5c47b3e5ec7",
+    "url": "/static/media/part10.d9f20caa.mp3"
+  },
+  {
+    "revision": "3097ec027164c619d31085dd8f0279a0",
+    "url": "/static/media/part2.3097ec02.mp3"
+  },
+  {
+    "revision": "d9c424af95c6fcfe70558fdb444d00ce",
+    "url": "/static/media/part3.d9c424af.mp3"
+  },
+  {
+    "revision": "730a696508b9a05cab11a36a2862ce30",
+    "url": "/static/media/part4.730a6965.mp3"
+  },
+  {
+    "revision": "cb710e0b0d113f4d607bcfec2b6b0ff1",
+    "url": "/static/media/part5.cb710e0b.mp3"
+  },
+  {
+    "revision": "59773c02a97f5d122ec085de87f70eb5",
+    "url": "/static/media/part6.59773c02.mp3"
+  },
+  {
+    "revision": "bfc49738baa2c637c1d066d78902de1b",
+    "url": "/static/media/part7.bfc49738.mp3"
+  },
+  {
+    "revision": "8833d65d208356cce4f6e16df958b31f",
+    "url": "/static/media/part8.8833d65d.mp3"
+  },
+  {
+    "revision": "7233439a29fd0b6c097f576fb292d131",
+    "url": "/static/media/part9.7233439a.mp3"
+  }
+]);
--- src/Album.js
+++ src/Album.js
@@ -115,7 +115,7 @@ export default function Album() {
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               Something short and leading about the collection below—its contents, the creator, etc.
               Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
+              entirely. Just Listen.
             </Typography>
             
           </Container>
